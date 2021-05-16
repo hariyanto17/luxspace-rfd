e48fd8f1053a44048431406251ed4e5aa8ee85d4
hariyanto17 <hariyanto17@mhs.akba.ac.id>
2021-05-16 12:08:43 +0800

    add useScrollAnchor

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,15 @@
 import Clients from "parts/Clients";
 import Footer from "parts/Footer";
 import Header from "parts/Header";
-import Hero from "parts/Hero";
+import Hero from "parts/HomePage/Hero";
 import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
 import Sitemap from "parts/Sitemap";
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
 
 const HomePage = () => {
+  useScrollAnchor();
+
   return (
     <>
       <Header theme="white" position="absolute" />
